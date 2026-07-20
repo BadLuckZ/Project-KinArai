22ca0d59eb9dac6ec36fdbf5ace2caf4ed5b08ec
BadLuckZ <thanagorn8802@gmail.com>
2026-07-20 15:03:26 +0700

    feat: add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import { Elysia } from "elysia";
 import { menuRoutes } from "./features/menu/menu.route";
 import { historyRoutes } from "./features/history/history.route";
+import cors from "@elysia/cors";
 
-const app = new Elysia().use(menuRoutes).use(historyRoutes).listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => {
+    return { status: true, timestamp: new Date().toISOString() };
+  })
+  .use(menuRoutes)
+  .use(historyRoutes)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
